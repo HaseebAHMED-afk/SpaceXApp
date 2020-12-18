7a985e563c1d1600826c40b76fa2ccd083a4663e
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-12-19 00:17:24 +0500

    Checkpoint at Home page

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -4,8 +4,8 @@ import './Home.css'
 const Home = () => {
     return (
         <div>
-            Home
-        </div>
+            <img className="spacex-img" src="https://www.teslarati.com/wp-content/uploads/2019/06/Starlink-coverage-Earth-SpaceX-2-crop.jpg" alt="spacex-img" />
+        </div> 
     )
 }
 
