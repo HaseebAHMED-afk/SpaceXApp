c04258302e509694503ce357d21fad38eb540db1
HaseebAHMED-afk <63052623+HaseebAHMED-afk@users.noreply.github.com>
2020-12-19 19:34:36 +0500

    Added First Section

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Button } from '@material-ui/core'
 import './Home.css'
 
 const Home = () => {
     return (
         <div>
             <img className="spacex-img" src="https://www.teslarati.com/wp-content/uploads/2019/06/Starlink-coverage-Earth-SpaceX-2-crop.jpg" alt="spacex-img" />
+            <div className="second-div">
+                <Button variant="contained" className="btn">See The Launch</Button>
+            </div>
         </div> 
     )
 }
